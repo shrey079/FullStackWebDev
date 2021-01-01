4a90ab3c5ec5e35a33e234746f20810e17b6996d
shrey079 <shreypatel079@gmail.com>
2021-01-01 13:30:52 -0500

    Final day 4 changes

--- Day 4/app.js
+++ Day 4/app.js
@@ -3,14 +3,14 @@ console.log(34);
 // het user prompt
 let ans = prompt("What is your fav day of the week?") 
 ans = ans.toLowerCase();
-console.log(typeof(ans));
+
 
 // handles empty input
 while(ans.length == 0){ 
     let ans = prompt("What is your fav day of the week?"); 
 }
 ans = ans.toLowerCase();  
-console.log(typeof(ans));
+
 
 // print based off value of ans
 if( ans === "monday"){ 
