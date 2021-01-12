d5a734ac6ad49fba3e2cd260151726770b4c5528
shrey079 <shreypatel079@gmail.com>
2021-01-11 23:25:31 -0500

    Learned more about events in Javascript

---
+++ Day 8/app.js
@@ -0,0 +1,7 @@
+const b2 = document.querySelector("#b2")
+b2.onclick = function() { 
+    console.log("The second button was clicked lmfao")
+}
+
+const b3 = document.querySelector("#b3")
+b3.addEventListener("click", ()  => { alert("Ur cool")})
