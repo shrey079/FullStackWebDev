9719a63b827606be82ff4595d7e1e2480edf2200
shrey079 <shreypatel079@gmail.com>
2021-01-18 02:48:35 -0500

    Commite message

--- First Express app/index.js
+++ First Express app/index.js
@@ -2,15 +2,30 @@ const express = require('express')
 const app = express(); 
 const port = 3000; 
 
-
+// // This method runs evertime we get any kind of request no matter the route of our re. 
+// app.use(() => { 
+//     console.log("We Got a request to our server.")
+// })
 
 // We return res, if someone makes a req to the path '/' on localhost:port
 app.get('/', (req, res) => { 
-    res.send('This is some text that I generated with the use of Express.')
+    res.send('This is the root route.')
 })
 
-app.use()
+// if someone request that route, then this is what we will send back. 
+app.get('/cats', (req, res) => { 
+    res.send('You navigated to the /cat page.')
+})
 
+app.get('*', (req, res) => { 
+    res.send("THIS RESOURCE DOES NOT EXIST!!")
+})
+
+app.post("/", (req, res) => { 
+    res.send("I got your post request")
+})
+
+// This ensures that our app listens for req's on the correct port.
 app.listen(port, () => { 
     console.log("Listening on Port 3000!")
 })
