c6732d07dad568f8405ae7125da8977bd2ccde2d
shrey079 <shreypatel079@gmail.com>
2021-01-21 02:35:48 -0500

    .

--- Templates/express_template.js
+++ Templates/express_template.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => { 
+    res.send("This is a response")
+})
+
 app.listen(3000, () => { 
     console.log("Listening to port 3000")
 })
