6e6240d6b447f31f94e451263b21e2f3f75db05f
shrey079 <shreypatel079@gmail.com>
2021-01-21 03:46:36 -0500

    Learned about how to make templates with EJS

--- TemplatingandEJS/index.js
+++ TemplatingandEJS/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 // Set the path to the place where index.js is run from
