e9725d1c8f5603f44082ac26e47cca70ca417289
shrey079 <shreypatel079@gmail.com>
2021-02-06 04:49:15 -0500

    .

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -30,3 +30,17 @@ app.get('/:trash', (req, res) => {
 app.listen(port, () => { 
     console.log("Listening on Port 3000!")
 })
+
+
+// Steps to host the website on your local machine
+// 1) Download node.js if you don't have it already
+// 2) Make a directory for hackathon
+// 3) Navigate to that hackathon directory in your terminal and then run "npm i express"
+// 4) make a directory inside that folder called 'public'
+// 5) Add your Html,css and js files
+// 6) Change the files name to index.html or about.html accordingly
+// 7) copy paste the code above into the /hackathon directory under a file named app.js
+// 8) Then uncomment the lines that say res.sendFile()
+// 9) go to terminal and make sure you're in the Uottahack directory
+// 10) run "node app.js"
+// 11) in chrome open "localhost::3000" and your website should be there
